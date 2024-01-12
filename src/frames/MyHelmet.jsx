c682810be2085dbc1f7fa71cbcc755ca4abfe2f2
unknown <gonzalezleonardoantonio056@gmail.com>
2024-01-12 17:39:30 -0300

    emoji in og title

--- src/frames/MyHelmet.jsx
+++ src/frames/MyHelmet.jsx
@@ -7,7 +7,7 @@ const MyHelmet = () => {
       {/* Resto de tu componente */}
       <Helmet>
         <title>Título de tu página</title>
-        <meta property="og:title" content="Portfolio - Leonardo" />
+        <meta property="og:title" content="💻 Portfolio - Leonardo" />
         <meta property="og:description" content="Sitio web personal" />
         <meta property="og:image" content="./../assets/Screenshot.PNG" />
         <meta property="og:url" content="https://leonantgon.github.io/react-portfolio-es/" />
