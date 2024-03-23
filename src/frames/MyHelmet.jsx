5e71e3c27e5cc65797f5b0e20f53e818646a916c
unknown <gonzalezleonardoantonio056@gmail.com>
2024-03-22 21:02:10 -0300

    my helmet og:url

--- src/frames/MyHelmet.jsx
+++ src/frames/MyHelmet.jsx
@@ -9,7 +9,7 @@ const MyHelmet = () => {
         <meta property="og:title" content="Portfolio - Leonardo" />
         <meta property="og:description" content="Sitio web personal" />
         <meta property="og:image" content="./../assets/Screenshot.PNG" />
-        <meta property="og:url" content="https://leonantgon.github.io/react-portfolio-es/" />
+        <meta property="og:url" content="https://leonardoweb.online/" />
       </Helmet>
     </div>
   );
